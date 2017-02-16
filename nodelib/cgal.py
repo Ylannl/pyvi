b67220a040a0b4a213f06175e36ef173b86de47c
Ravi Peters <r.y.peters@tudelft.nl>
2017-02-16 21:13:02 +0100

    simplify cgal iterator

--- nodelib/cgal.py
+++ nodelib/cgal.py
@@ -34,41 +34,30 @@ class cgalTriangleExpanderNode(Node):
         })
 
     def process(self, triangulation_3, display=True):
-        fit = triangulation_3.finite_facets()
-        
         nf = triangulation_3.number_of_facets()
 
         triangles = np.empty((3*nf,3), dtype=np.float32)
         normals = np.empty((nf,3), dtype=np.float32)
         
-        if fit.hasNext():
-            # done = fit.next()
-            facet = Delaunay_triangulation_3_Facet()
-            j=0
-            k=0
-            while(1):
-                facet=fit.next()
-                cell, i = facet
-                for vid in self.vertexid_map[i]:
-                    p = cell.vertex(vid).point()
-                    triangles[j,0] = p.x()
-                    triangles[j,1] = p.y()
-                    triangles[j,2] = p.z()
-                    j+=1
-                p1=triangles[-3]
-                p2=triangles[-2]
-                p3=triangles[-1]
-                v=p2-p1
-                w=p3-p1
-                normals[k]= np.array([
-                        v[1]*w[2]-v[2]*w[1],
-                        v[2]*w[0]-v[0]*w[2],
-                        v[0]*w[1]-v[1]*w[0] ])
-                k+=1
-                # if facet == done:
-                if not fit.hasNext():
-                    break
+        j=0
+        k=0
+        for facet in triangulation_3.finite_facets():
+            cell, i = facet
+            for vid in self.vertexid_map[i]:
+                p = cell.vertex(vid).point()
+                triangles[j,0] = p.x()
+                triangles[j,1] = p.y()
+                triangles[j,2] = p.z()
+                j+=1
+            p1=triangles[-3]
+            p2=triangles[-2]
+            p3=triangles[-1]
+            v=p2-p1
+            w=p3-p1
+            normals[k]= np.array([
+                    v[1]*w[2]-v[2]*w[1],
+                    v[2]*w[0]-v[0]*w[2],
+                    v[0]*w[1]-v[1]*w[0] ])
+            k+=1
 
         return {'triangles':triangles, 'normals':normals}
-        
-        
