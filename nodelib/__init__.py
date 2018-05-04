8a8c8c42b450634c27afcb36787d204282a1e190
Ravi Peters <r.y.peters@tudelft.nl>
2018-05-04 14:45:05 +0200

    don't crash if cgal is missing

--- nodelib/__init__.py
+++ nodelib/__init__.py
@@ -2,8 +2,15 @@ from pyqtgraph.flowchart.NodeLibrary import NodeLibrary, isNodeClass
 from pyqtgraph.reload import reload as pqtgreload
 
 # nodetypes
-from . import pyvi, skel3d, misc, cgal
+from . import pyvi, skel3d, misc
 from pyqtgraph.flowchart.library import Operators, Display, Data
+nodemods = [pyvi, skel3d, misc, Operators, Display, Data]
+try:
+    from . import cgal
+    nodemods.append(cgal)
+except:
+    print('could not load cgal nodes')
+    pass
 
 class MyNodeLib(NodeLibrary):
 
@@ -16,7 +23,7 @@ class MyNodeLib(NodeLibrary):
 
 LIBRARY = MyNodeLib()
 # Add all nodes to the default library
-for mod in [pyvi, skel3d, misc, cgal, Operators, Display, Data]:
+for mod in nodemods:
     nodes = [getattr(mod, name) for name in dir(mod) if isNodeClass(getattr(mod, name))]
     for node in nodes:
         LIBRARY.addNodeType(node, [(mod.__name__.split('.')[-1],)])
